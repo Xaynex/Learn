a1625235b5464fdee55836bff58d7d5ef30c4c0a
Saynex <hrabrov1998@mail.ru>
2022-09-19 00:17:06 +0300

    9 задача дз 16 модуля

--- 16 module/16.py
+++ 16 module/16.py
@@ -234,3 +234,25 @@
 #             print('не подошло')
 #
 # print(count)
+
+#задача 9
+# friend = int(input('Кол-во друзей: '))
+# receipt = int(input('Долговых расписок: '))
+# friends_list = []
+#
+# for _ in range(friend):
+#     friends_list.append(0)
+#
+# for number in range(receipt):
+#     print(f'\n{number + 1} расписка')
+#     to_who = int(input('Кому: '))
+#     from_who = int(input('От кого: '))
+#     how_much = int(input('Сколько: '))
+#     friends_list[from_who - 1] += how_much
+#     friends_list[to_who - 1] -= how_much
+#
+# print('\nБаланс друзей:')
+# for index in range(len(friends_list)):
+#     print(f'{index + 1} : {friends_list[index]}')
+
+
