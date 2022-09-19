61f3ffb32aab547da90cbc10a4b5c931503cac87
Дмитрий (работа) <hrabrov1998@mail.ru>
2022-09-19 10:34:33 +0300

    решил 3 задачу 17 модуля

--- 17 module/17.py
+++ 17 module/17.py
@@ -15,5 +15,12 @@
 # list_1=[x+x+sym for x in stroka]
 # print(list_1)
 
-
-
+# Задача 3. Повышение цен
+# items=[1.09,23.56,57.84,4.56,6.78]
+#
+# year_1=int(input('повышение 1 год: '))
+# year_2=int(input('повышение 2 год: '))
+#
+# total=year_1+year_2
+# items_new=[round(i+(total/100*i),2) for i in items]
+# print(items_new)
