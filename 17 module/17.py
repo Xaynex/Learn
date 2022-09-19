b48bfe9362c2cd645792224fb16e7112d3672f69
Дмитрий (работа) <hrabrov1998@mail.ru>
2022-09-19 10:05:41 +0300

    решил 2 задачу 17 модуля

--- 17 module/17.py
+++ 17 module/17.py
@@ -1,1 +1,9 @@
-# задача 1
+# задача 2
+
+# stroka=input('введите строку ')
+# sym=input('введите символ ')
+#
+# list_1=[x+x for x in stroka]
+# print(list_1)
+# list_1=[x+x+sym for x in stroka]
+# print(list_1)
