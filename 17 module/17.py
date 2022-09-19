0ff4b4ac7b1b35d4b1b5c02a0ea0a7817bf21423
Дмитрий (работа) <hrabrov1998@mail.ru>
2022-09-19 09:50:41 +0300

    решил 1 задачу 17 модуля

--- 17 module/17.py
+++ 17 module/17.py
@@ -1,1 +1,7 @@
-# задача 1
+# Задача 1
+# L=int(input('левая граница '))
+# R=int(input('правая граница '))
+# left_list=[x**3 for x in range(L,R+1) ]
+# print(left_list)
+# Right_list=[x**2 for x in range(L,R+1) ]
+# print(Right_list)
