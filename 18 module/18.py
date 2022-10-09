6efcd914fcf4c8c06051a9ebb4e97e222fc8de89
Saynex <hrabrov1998@mail.ru>
2022-10-09 23:21:15 +0500

    сделал 18.2

---
+++ 18 module/18.py
@@ -0,0 +1,32 @@
+# Задача 1.1 Заказ
+# name= input('Имя: ')
+# number=input('Номер заказа: ')
+#
+# message='Здравствуйте, {name}! Ваш номер заказа: {number}. Приятного дня!'.format(name=name, number=number)
+# print(message)
+
+# Задача 1.2 Долги
+
+# name= input('Имя: ')
+# Debt=input('Долг: ')
+#
+# message='{0}! {0}, привет! Как дела, {0}? Где мои {1} рублей? {0}!'.format(name, Debt)
+# print(message)
+
+# Задача 1.3 IP-адрес
+# def check(a):
+#     if a > 255 or a < 0:
+#         print('ошибка')
+#         quit()
+#
+# a0 = int(input())
+# check(a0)
+# a1 = int(input())
+# check(a1)
+# a2 = int(input())
+# check(a2)
+# a3 = int(input())
+# check(a3)
+# ip_address = '{0}.{1}.{2}.{3}'.format(a0, a1, a2, a3)
+# print(ip_address)
+
